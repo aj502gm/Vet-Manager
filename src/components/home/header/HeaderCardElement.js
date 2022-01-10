31d257d7944fb59ca65ac4512711029adb18f0ac
aj502gm <20004118@galileo.edu>
2022-01-10 15:31:48 -0600

    Pets Cards display still in progress

--- src/components/home/header/HeaderCardElement.js
+++ src/components/home/header/HeaderCardElement.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import "../../../css/headerElements.css";
+import {ProductCard} from "react-ui-cards"
 function HeaderCardElement() {
     return (
-        <div className = "header-element">
-            1
-        </div>
+        <ProductCard photos = {['https://i.imgur.com/t7DTziH.jpg','https://i.imgur.com/t7DTziH.jpg', 'https://i.imgur.com/t7DTziH.jpg' ]} price = "Q10" productName = "COLIN" description = "LOREM FDSFSDKAFHSDJAHFJSD" buttonText = "Change"></ProductCard>
     )
 }
 
